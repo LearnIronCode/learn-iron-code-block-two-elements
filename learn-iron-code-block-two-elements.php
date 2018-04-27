de8434b5afaf9c8f045140e3f473dff1ae3814a3
Sal Ferrarello <sal@ironcodestudio.com>
2018-04-27 14:28:44 -0400

    Initial commit
    - Generated files from WP CLI scaffold plugin
    - Generated files from WP CLI scaffold block
    - README.md
    - LICENSE.txt

---
+++ learn-iron-code-block-two-elements.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Learn Iron Code Block Two Elements
+ * Plugin URI:      https://salferrarello.com/wordpress-custom-editor-block/
+ * Description:     Gutenberg Block Example
+ * Author:          Sal Ferrarello
+ * Author URI:      https://salferrarello.com/
+ * Text Domain:     learn-iron-code-block-two-elements
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Learn_Iron_Code_Block_Two_Elements
+ */
+
+// Your code starts here.
