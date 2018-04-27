08eeb821e509a9e3d6e83def2a253bdce31ab354
Sal Ferrarello <sal@ironcodestudio.com>
2018-04-27 14:31:53 -0400

    Include PHP files to enable block
    At this point, you can add the "Iron Code Two Elements" block in Gutenberg.

--- blocks/iron-code-two-elements.php
+++ blocks/iron-code-two-elements.php
@@ -13,6 +13,13 @@
  * @see https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type/#enqueuing-block-scripts
  */
 function iron_code_two_elements_block_init() {
+
+	// Until Gutenberg is merged into WordPress core, register_block_type()
+	// will only exist when the Gutenberg plugin is installed and activated.
+	if ( ! function_exists( 'register_block_type' ) ) {
+		return;
+	}
+
 	$dir = dirname( __FILE__ );
 
 	$index_js = 'iron-code-two-elements/index.js';
--- learn-iron-code-block-two-elements.php
+++ learn-iron-code-block-two-elements.php
@@ -12,4 +12,9 @@
  * @package         Learn_Iron_Code_Block_Two_Elements
  */
 
-// Your code starts here.
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+include( plugin_dir_path( __FILE__ ) . 'blocks/iron-code-two-elements.php' );
